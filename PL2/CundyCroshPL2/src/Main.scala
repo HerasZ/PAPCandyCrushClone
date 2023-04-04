b7be5494a8096b917d75aad526c52548ea8ae31c
adriibc <adrian.borges@edu.uah.es>
2023-04-04 21:59:05 +0200

    Creación PL2

---
+++ PL2/CundyCroshPL2/src/Main.scala
@@ -0,0 +1,20 @@
+object Main {
+  def main(args: Array[String]): Unit = {
+
+    val numFilas: Int = 0
+    val numColumnas: Int = 0
+
+    //TODO: generarTablero
+
+    //TODO: activarBomba
+
+    //TODO: activarRompecabezas
+
+    //TODO: activarTNT
+
+    //TODO: eliminarBloques
+
+    //TODO: elegirBloqueAutomatico
+
+  }
+}
