be0dd32c756c023e56c83641f374a179fd39454b
adriibc <adrian.borges@edu.uah.es>
2023-04-16 20:03:41 +0200

    Cambios menores de la interfaz

--- PL2/CundyCroshPL2InterfazSinOptimizado/src/ventanaInicial.java
+++ PL2/CundyCroshPL2InterfazSinOptimizado/src/ventanaInicial.java
@@ -10,49 +10,49 @@ public class ventanaInicial extends JFrame implements ActionListener {
 
     public ventanaInicial() {
 
-        // Establecer las propiedades de la ventana
+        // Establecemos las propiedades de la ventana
         setTitle("Cundy Crosh PAP-2023");
         setSize(700, 400);
-        setLocationRelativeTo(null); // Centrar la ventana en la pantalla
+        setLocationRelativeTo(null); // Centramos la ventana en la pantalla
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Hacer que la ventana no sea redimensionable
+        // Hacemos que la ventana no sea redimensionable
         setResizable(false);
 
-        // Crear un panel para colocar los componentes
+        // Creamos un panel para colocar los componentes
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-// Crear el ImageIcon
-        ImageIcon imagenFondo = new ImageIcon("C:\\Users\\UAH\\Desktop\\fondoPantallaCandyCrushOscuro.png");      //TODO: Si funciona, cambiarlo por la ruta relativa del proyecto
+        // Creamos el ImageIcon
+        ImageIcon imagenFondo = new ImageIcon("src/fondoPantallaCandyCrushOscuro.png");
 
-// Cargar la imagen y crear un objeto JLabel con la imagen
+        // Cargamos la imagen y creamos un objeto JLabel con la imagen
         JLabel labelFondo = new JLabel(imagenFondo);
-// Agregar el JLabel al panel y establecer su posición
+        // Agregamos el JLabel al panel y establecemos su posición
         labelFondo.setBounds(0, 0, imagenFondo.getIconWidth(), imagenFondo.getIconHeight());
         panel.add(labelFondo);
 
-        // Crear un label con el nombre
+        // Creamos un label para cada componente
         JLabel titulo = new JLabel("Bienvenido a Cundy Crush!");
         JLabel autores = new JLabel("By: DHZ y ABC");
         titulo.setFont(new Font("Arial", Font.BOLD, 30));
         titulo.setForeground(Color.WHITE);
         autores.setFont(new Font("Tahoma", Font.BOLD, 12));
         autores.setForeground(Color.WHITE);
-        // Obtener el tamaño de la ventana y del titulo
+        // Obtenemos el tamaño de la ventana y del titulo
         Dimension tamañoVentana = getSize();
         Dimension tamañoTexto = titulo.getPreferredSize();
-        // Calcular la posición del titulo
+        // Calculamos la posición del titulo
         int x = (tamañoVentana.width - tamañoTexto.width) / 2;
         int y = (tamañoVentana.height - tamañoTexto.height) / 2;
-        // Establecer la posición del titulo en el panel
+        // Establecemos la posición de los textos en el panel
         titulo.setBounds(x, y-90, tamañoTexto.width+20, tamañoTexto.height);
         autores.setBounds(x+145, y-60, tamañoTexto.width+20, tamañoTexto.height);
-        // Agregar el titulo al panel
+        // Agregamos el titulo y el subtitulo al panel
         labelFondo.add(titulo);
         labelFondo.add(autores);
 
-        // Crear un botón para redirigir a la siguiente ventana
+        // Creamos un botón para redirigir a la siguiente ventana
         botonAdelante = new JButton("Adelante");
         botonAdelante.addActionListener(this); // Agregar un ActionListener
         botonAdelante.setBounds(x+92, y+40, 200, 50);
@@ -61,13 +61,13 @@ public class ventanaInicial extends JFrame implements ActionListener {
         botonAdelante.setForeground(Color.WHITE);
         labelFondo.add(botonAdelante);
 
-        // Agregar el panel a la ventana
+        // Agregamos el panel a la ventana
         getContentPane().add(panel);
     }
 
-    // Implementar el método actionPerformed para el botón
+    //Establecemos lo que debe ocurrir cuando pulsemos el boton
     public void actionPerformed(ActionEvent e) {
-        // Si el botón es pulsado, abrir la siguiente ventana
+        // Si el botón es pulsado, abrimos la siguiente ventana
         if (e.getSource() == botonAdelante) {
             ventanaDatos ventanaNueva = new ventanaDatos();
             ventanaNueva.setVisible(true);
